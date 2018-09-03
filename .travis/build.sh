ebc66ec0b6d864499f181d85e7f6221f2d8e7f2f
Simon Woodman <swoodman@redhat.com>
2018-09-03 11:37:03 +0100

    Travis integration

---
+++ .travis/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+    echo "Building Pull Request - nothing to push"
+else
+    echo "Login into Docker Hub ..."
+    echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin docker.io
+
+    docker build -t docker.io/streamziprocessors/cef-ops-log-data:latest log-data
+    docker push docker.io/streamziprocessors/cef-ops-log-data:latest
+
+    docker build -t docker.io/streamziprocessors/cef-ops-random-data:latest random-data
+    docker push docker.io/streamziprocessors/cef-ops-random-data:latest
+
+    docker build -t docker.io/streamziprocessors/cef-ops-node-filter-event-data:latest nodejs/filter-events
+    docker push docker.io/streamziprocessors/cef-ops-node-filter-event-data:latest
+
+    docker build -t docker.io/streamziprocessors/cef-ops-receiver:latest amqp/receiver
+    docker push docker.io/streamziprocessors/cef-ops-receiver:latest
+
+    docker build -t docker.io/streamziprocessors/cef-ops-sender:latest amqp/receiver
+    docker push docker.io/streamziprocessors/cef-ops-sender:latest
+
+fi

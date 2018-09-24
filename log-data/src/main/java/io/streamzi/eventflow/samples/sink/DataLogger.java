9997fb2d06d06e7ae6d9d95187228a869a94287c
Matthias Wessendorf <matzew@apache.org>
2018-09-24 14:26:28 +0200

    Change logger/sink package code

--- log-data/src/main/java/io/streamzi/eventflow/samples/sink/DataLogger.java
+++ log-data/src/main/java/io/streamzi/eventflow/samples/sink/DataLogger.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package io.streamzi.cloudeventflow.log.data;
+package io.streamzi.eventflow.samples.sink;
 
 import io.streamzi.cloudevents.CloudEvent;
 import io.streamzi.eventflow.annotations.CloudEventComponent;
